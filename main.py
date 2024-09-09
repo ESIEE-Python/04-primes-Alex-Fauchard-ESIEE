d9aa4804bc6445b7918f67af018cfe7b80346ef2
Alex Fauchard - ESIEE <alex.fauchard@edu.esiee.fr>
2024-09-09 20:56:03 +0000

    Voici mon programme sur les nombres premiers avec l'utilisation des fonctions principale et secondaire.

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
+""" Le module math fourni la fonction racine carré. """
 from math import sqrt
+
 
 #### Fonction secondaire
 
 
-def isprime(p):
+def isprime(p:int)->bool:
+    """
+    Objectif : la fonction cherche à déterminer si p est un nombre premier.
 
-    # votre code ici
+    Argument : un nombre quelconque.
 
-    pass
+    Retourne : un booléen qui est True si p est premier et False sinon.
+    """
+
+    a=int(sqrt(p)) # On assigne la variable a à la racine carré de p.
+
+    if p in (0,1) : # Cas spécifiques de 0 et 1.
+        return False
+
+    for i in range (2,a+1): # On essaie pour tous les nombres entre 2 et racine de p.
+        if p%i==0: # On cherche à savoir si le reste de la division entre p et i est nul.
+            return False # p n'est pas premier.
+    return True # p est premier.
+
 
 #### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """
+    Renvoi les nombres premiers qui sont dans les 100 premiers nombres entiers (0,1,2,3,...,98,99)
+    """
     for n in range(100):
-        if isprime(n):
+        if isprime(n) is True: # On vérifie si le nombre est premier avec la fonction isprime.
             print(n, end=", ")
 
     print()
-
 
 if __name__ == "__main__":
     main()
